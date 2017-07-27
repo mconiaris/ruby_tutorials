621d644e40bb5cbbacdc1e3b1cfab46f62034040
mconiaris <mconiaris@gmail.com>
2017-07-27 15:17:14 -0400

    Added class definition to cbrt example result.

--- math.rb
+++ math.rb
@@ -2,3 +2,5 @@ puts "This Math tutorial is based on Ruby version 2.4.1"
 
 puts "cbrt(x) returns the cube root of x."
 puts "Math.cbrt(9) = " + Math.cbrt(9).to_s
+c = Math.cbrt(9).class.to_s
+puts Math.cbrt(9).to_s + " is a " + c + "."
