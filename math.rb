8a588de40036d436f8dfbdd6203af1c2abaa6230
mconiaris <mconiaris@gmail.com>
2017-07-28 16:45:37 -0400

    Added exp(x) example.

--- math.rb
+++ math.rb
@@ -5,3 +5,7 @@ puts "Math.cbrt(9) = " + Math.cbrt(9).to_s
 c = Math.cbrt(9).class.to_s
 puts Math.cbrt(9).to_s + " is a " + c + "."
 print "\n"
+puts "exp(x) returns  e**x."
+puts "exp(4) = " + Math.exp(4).to_s
+c = Math.exp(4).class.to_s
+puts Math.exp(4).to_s + " is a " + c + "."
