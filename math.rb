45ad4b64052d867275407f423628f22dcde19325
mconiaris <mconiaris@gmail.com>
2017-07-31 14:34:34 -0400

    Added an additional newline.

--- math.rb
+++ math.rb
@@ -4,7 +4,7 @@ puts "cbrt(x) returns the cube root of x."
 puts "Math.cbrt(9) = " + Math.cbrt(9).to_s
 c = Math.cbrt(9).class.to_s
 puts Math.cbrt(9).to_s + " is a " + c + "."
-print "\n"
+print "\n\n"
 puts "exp(x) returns  e**x."
 puts "exp(4) = " + Math.exp(4).to_s
 c = Math.exp(4).class.to_s
