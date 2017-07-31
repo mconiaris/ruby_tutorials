573ea5c8d2f919d9d47c9712ad5811350c0c20b7
mconiaris <mconiaris@gmail.com>
2017-07-31 14:38:17 -0400

    Added square root example.

--- math.rb
+++ math.rb
@@ -9,3 +9,8 @@ puts "exp(x) returns  e**x."
 puts "exp(4) = " + Math.exp(4).to_s
 c = Math.exp(4).class.to_s
 puts Math.exp(4).to_s + " is a " + c + "."
+print "\n\n"
+puts "sqrt(x) returns  the non-negative square root of x."
+puts "sqrt(4) = " + Math.sqrt(4).to_s
+c = Math.sqrt(4).class.to_s
+puts Math.sqrt(4).to_s + " is a " + c + "."
