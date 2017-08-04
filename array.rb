07e836fbfd6c85a6702427ee3544b6dbdc113905
mconiaris <mconiaris@gmail.com>
2017-08-04 13:10:48 -0400

    Puts ary example.

--- array.rb
+++ array.rb
@@ -1,2 +1,4 @@
 puts "This Array tutorial is based on Ruby version 2.4.1"
 puts "To create an array, you can use this syntax: \n\"ary = [1, 'two', 3.0] #=> [1, 'two', 3.0]\""
+ary = [1, "two", 3.0] #=> [1, "two", 3.0]
+puts ary
