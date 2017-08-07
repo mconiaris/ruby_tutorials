e2ea0f6e3bce6a22a71229f8edb7f2bb4f036b6e
mconiaris <mconiaris@gmail.com>
2017-08-07 11:53:10 -0400

    Added array notes.

--- array.rb
+++ array.rb
@@ -3,3 +3,4 @@ puts "To create an array, you can use this syntax: \n\"ary = [1, 'two', 3.0] #=>
 ary = [1, "two", 3.0] #=> [1, "two", 3.0]
 puts ary
 puts "\n"
+puts "An array can also be created by explicitly calling ::new with zero, one (the initial size of the Array) or two arguments (the initial size and a default object)."
