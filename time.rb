9e1d673aa3ea786cbc177d389a7f0ae701e5c07a
mconiaris <mconiaris@gmail.com>
2017-08-21 20:13:37 -0400

    Created a Time example.

--- time.rb
+++ time.rb
@@ -1,3 +1,5 @@
 puts "This Time tutorial is based on Ruby version 2.4.1"
 puts "\n"
 puts "You can create a new instance of Time with ::new. This will use the current system time. ::now is an alias for this. You can also pass parts of the time to ::new such as year, month, minute, etc. When you want to construct a time this way you must pass at least a year. If you pass the year with nothing else time will default to January 1 of that year at 00:00:00 with the current system timezone."
+t = Time.new
+puts "This is an eample of a time object being instantiated with \"Time.new\": " + t.to_s
